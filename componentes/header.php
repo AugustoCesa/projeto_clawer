844f1867778db81513aa91ff5b0a6c465e25c920
Augusto <augusto.boebel@alunos.sc.senac.br>
2022-08-23 17:04:01 -0300

    atualizando

--- componentes/header.php
+++ componentes/header.php
@@ -9,6 +9,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body style="background-image: linear-gradient(white, #000000);">
+<body style="background-image: linear-gradient(#2046b0,#101c66);">
     
 
--- intAdm.php
+++ intAdm.php
@@ -13,22 +13,23 @@
 
 
 
-    <h1 style="color: black; text-align:center">Interface de administrador</h1>
-
-    <div style="display: flex; justify-content:center; margin-top: 100px ">
+    <h1 style="color: black; text-align:left; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif">Interface de administrador</h1>
+    <hr>
+    <div style="display: flex; justify-content:left; margin-top: 100px; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif ">
         <div style="display: flex; flex-direction:column">
-            <h3 style="color: black;">Cadastrar Carro</h3>
-            <button type="button" style="width: 80px;"><a href="CadCarros.php" style="text-decoration:none; color: black;">entrar</a></button>
-            <h3 style="color: black;">Mudar informações dos carros</h3>
-            <button type="button" href="Altcarro.php" style="width: 80px;"><a href="AltCarro.php" style="text-decoration:none; color: black;">entrar</a></button>
-
-            <div style="border: solid black 2px;">
+            <div style="border: solid black 2px; border-radius:8px; margin-top: 40px; text-align:center;height:190px; width: 400px">
+                <h3 style="color: black;">Cadastrar Carro</h3>
+                <button type="button" style="width: 80px; height:30px; background-color:gray;"><a href="CadCarros.php" style="text-decoration:none; color: white; font-size:18px">entrar</a></button>
+                <h3 style="color: black;">Mudar informações dos carros</h3>
+                <button type="button" href="Altcarro.php" style="width: 80px;height:30px; background-color:gray; "><a href="AltCarro.php" style="text-decoration:none; background-color:gray; color:white; font-size:18px">entrar</a></button>
+            </div>
+            <div style="border: solid black 2px; border-radius:8px; margin-top: 40px; text-align:center; height:190px">
                 <h2 style="color: black;">Usuário</h2>
                 <h3 style="color: black;">Alterar usuários</h3>
-                <button type="button" href="AltUsuario.php" style="width: 80px;"><a href="AltUsuarios.php" style="text-decoration:none; color: black;">entrar</a></button>
+                <button type="button" href="AltUsuario.php" style="width: 80px;height:30px; background-color:gray; "><a href="AltUsuarios.php" style="text-decoration:none; color: white; font-size:18px">entrar</a></button>
             </div>
 
-
+            <h3>voltar para o inicio</h3>
 
         </div>
     </div>
