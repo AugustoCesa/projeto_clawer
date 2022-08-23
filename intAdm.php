c650b0f6a0f55f2549de66046878b62f2859aacf
Augusto <augusto.boebel@alunos.sc.senac.br>
2022-08-23 16:22:21 -0300

    sei lá

--- intAdm.php
+++ intAdm.php
@@ -8,23 +8,23 @@
     <title>Document</title>
 </head>
 
-<body style="background-color: black;">
+<body style="background-color: white;">
 
 
 
 
-    <h1 style="color: white; text-align:center">Interface de administrador</h1>
+    <h1 style="color: black; text-align:center">Interface de administrador</h1>
 
     <div style="display: flex; justify-content:center; margin-top: 100px ">
         <div style="display: flex; flex-direction:column">
-            <h3 style="color: white;">Cadastrar Carro</h3>
+            <h3 style="color: black;">Cadastrar Carro</h3>
             <button type="button" style="width: 80px;"><a href="CadCarros.php" style="text-decoration:none; color: black;">entrar</a></button>
-            <h3 style="color: white;">Mudar informações dos carros</h3>
+            <h3 style="color: black;">Mudar informações dos carros</h3>
             <button type="button" href="Altcarro.php" style="width: 80px;"><a href="AltCarro.php" style="text-decoration:none; color: black;">entrar</a></button>
 
-            <div>
-                <h2 style="color: white;">Usuário</h2>
-                <h3 style="color: white;">Alterar usuários</h3>
+            <div style="border: solid black 2px;">
+                <h2 style="color: black;">Usuário</h2>
+                <h3 style="color: black;">Alterar usuários</h3>
                 <button type="button" href="AltUsuario.php" style="width: 80px;"><a href="AltUsuarios.php" style="text-decoration:none; color: black;">entrar</a></button>
             </div>
 
