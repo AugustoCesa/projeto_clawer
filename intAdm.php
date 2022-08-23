9b68214206dfb183dd77ac2d7ea8f641a3a5ed42
Augusto Cesar <augustoboebel@gmail.com>
2022-08-23 09:16:37 -0300

    primeiros passos

---
+++ intAdm.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="background-color: black;">
+
+
+
+
+    <h1 style="color: white; text-align:center">Interface de administrador</h1>
+
+    <div style="display: flex; justify-content:center; margin-top: 100px ">
+        <div style="display: flex; flex-direction:column">
+            <h3 style="color: white;">Cadastrar Carro</h3>
+            <button type="button" style="width: 80px;"><a href="CadCarros.php" style="text-decoration:none; color: black;">entrar</a></button>
+            <h3 style="color: white;">Mudar informações dos carros</h3>
+            <button type="button" href="Altcarro.php" style="width: 80px;"><a href="AltCarro.php" style="text-decoration:none; color: black;">entrar</a></button>
+
+            <div>
+                <h2 style="color: white;">Usuário</h2>
+                <h3 style="color: white;">Alterar usuários</h3>
+                <button type="button" href="AltUsuario.php" style="width: 80px;"><a href="AltUsuarios.php" style="text-decoration:none; color: black;">entrar</a></button>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
