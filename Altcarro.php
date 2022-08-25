645aa497277a2832303a4b51538f259fcc9918c9
Augusto <augusto.boebel@alunos.sc.senac.br>
2022-08-25 17:04:05 -0300

    atualizando

--- Altcarro.php
+++ Altcarro.php
@@ -1,6 +1,6 @@
 <?php
-include "../include/MySql.php";
-$codigo="";
+include "include/MySql.php";
+$codigo = "";
 $marca = "";
 $nome = "";
 $preco = "";
@@ -25,7 +25,7 @@ $combustivelErro = "";
 $kilometragemErro = "";
 $placaErro = "";
 $corErro = "";
-$modeloErro="";
+$modeloErro = "";
 $msgErro = "";
 
 if (isset($_GET['id'])) {
@@ -47,7 +47,6 @@ if (isset($_GET['id'])) {
             $cor = $value['cor'];
             $modelo = $value['modelo'];
             $imgContent = $value['imagem'];
-         
         }
     }
 }
@@ -72,35 +71,42 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
     if (empty($_POST['ano']))
         $anoErro = "ano é obrigatório!";
     else
-        $ano= $_POST['ano'];
+        $ano = $_POST['ano'];
 
-        if (empty($_POST['cambio']))
+    if (empty($_POST['cambio']))
         $cambioErro = "cambio é obrigatório!";
     else
-        $cambio= $_POST['cambio'];
+        $cambio = $_POST['cambio'];
 
-        if (empty($_POST['portas']))
+    if (empty($_POST['portas']))
         $portasErro = "portas é obrigatório!";
     else
-        $portas= $_POST['portas'];
+        $portas = $_POST['portas'];
 
-        if (empty($_POST['combustivel']))
+    if (empty($_POST['combustivel']))
         $combustivelErro = "combustivel é obrigatório!";
     else
-        $combustivel= $_POST['combustivel'];
+        $combustivel = $_POST['combustivel'];
 
-        if (empty($_POST['kilometragem']))
+    if (empty($_POST['kilometragem']))
         $kilometragemErro = "kilometragem é obrigatório!";
     else
-        $kilometragem= $_POST['kilometragem'];
+        $kilometragem = $_POST['kilometragem'];
 
-        if (empty($_POST['ano']))
-        $anoErro = "ano é obrigatório!";
+    if (empty($_POST['placa']))
+        $placaErro = "placa é obrigatório!";
     else
-        $ano= $_POST['ano'];
+        $placa = $_POST['placa'];
 
+    if (empty($_POST['cor']))
+        $corErro = "cor é obrigatório!";
+    else
+        $cor = $_POST['cor'];
 
-
+    if (empty($_POST['modelo']))
+        $modeloErro = "modelo é obrigatório!";
+    else
+        $modelo = $_POST['modelo'];
 
     if (!empty($_FILES["image"]["name"])) {
         //Pegar informações do arquivo
@@ -116,21 +122,28 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
         }
     }
 
-    if ($email && $nome && $senha && $telefone) {
+    if ($marca && $nome && $preco && $modelo) {
         //Verificar se ja existe o email
-        $sql = $pdo->prepare("SELECT * FROM USUARIO WHERE email = ? AND codigo <> ?");
-        if ($sql->execute(array($email, $codigo))) {
+        $sql = $pdo->prepare("SELECT * FROM CARROS WHERE modelo = ? AND CodCarro <> ?");
+        if ($sql->execute(array($modelo, $codigo))) {
             if ($sql->rowCount() <= 0) {
-                $sql = $pdo->prepare("UPDATE USUARIO SET codigo=?, 
+                $sql = $pdo->prepare("UPDATE CARROS SET CodCarro=?, 
+                                                             marca=?, 
                                                              nome=?, 
-                                                             email=?, 
-                                                             telefone=?, 
-                                                             senha=?,
-                                                             administrador=?,
+                                                             preco=?, 
+                                                             ano=?,
+                                                             cambio =?,
+                                                             portas = ?,
+                                                             combustivel = ?,
+                                                             kilometragem = ?,
+                                                             placa = ?,
+                                                             cor = ?,
+                                                             modelo = ?, 
                                                              imagem=?
-                                                       WHERE codigo=?");
+                                                       WHERE CodCarro=?");
 
-                if ($sql->execute(array($codigo, $nome, $email, $telefone, md5($senha), $administrador, $imgContent, $codigo))) {
+                if ($sql->execute(array( 
+                    $codigo,$marca, $nome, $preco, $ano,$cambio,$portas, $combustivel, $kilometragem, $placa, $cor, $modelo,$imgContent))) {
                     $msgErro = "Dados alterados com sucesso!";
                     header('location:listUsuario.php');
                 } else {
@@ -163,20 +176,40 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
         <fieldset>
             <legend>Cadastro de Usuário</legend>
 
-            Nome: <input type="text" name="nome" value="<?php echo $nome ?>">
+            marca: <input type="text" name="marca" value="<?php echo $marca ?>">
+            <span class="obrigatorio">*<?php echo $marcaErro ?></span>
+            <br>
+            nome: <input type="text" name="nome" value="<?php echo $nome ?>">
             <span class="obrigatorio">*<?php echo $nomeErro ?></span>
             <br>
-            Email: <input type="text" name="email" value="<?php echo $email ?>">
-            <span class="obrigatorio">*<?php echo $emailErro ?></span>
+            Preço: <input type="text" name="preco" value="<?php echo $preco ?>">
+            <span class="obrigatorio">*<?php echo $precoErro ?></span>
+            
             <br>
-            Telefone: <input type="text" name="telefone" value="<?php echo $telefone ?>">
-            <span class="obrigatorio">*<?php echo $telefoneErro ?></span>
+            ano: <input type="text" name="ano" value="<?php echo $ano ?>">
+            <span class="obrigatorio">*<?php echo $anoErro ?></span>
+          
             <br>
-            Senha: <input type="password" name="senha" value="<?php echo $senha ?>">
-            <span class="obrigatorio">*<?php echo $senhaErro ?></span>
+            cambio: <input type="text" name="cambio" value="<?php echo $cambio ?>">
+            <span class="obrigatorio">*<?php echo $cambioErro ?></span>
+
             <br>
-            <input type="checkbox" name="administrador">Administrador
+            portas: <input type="text" name="portas" value="<?php echo $portas ?>">
+            <span class="obrigatorio">*<?php echo $portasErro ?></span>
+
             <br>
+            combustivel: <input type="text" name="combustivel" value="<?php echo $combustivel ?>">
+            <span class="obrigatorio">*<?php echo $combustivelErro ?></span>
+
+            <br>
+            kilometragem: <input type="text" name="kilometregem" value="<?php echo $kilometragem?>">
+            <span class="obrigatorio">*<?php echo $kilometragemErro ?></span>
+
+            <br>
+            placa: <input type="text" name="placa" value="<?php echo $placa ?>">
+            <span class="obrigatorio">*<?php echo $placaErro ?></span>
+
+
             <input type="file" name="image">
             <br>
             <input type="submit" value="Salvar" name="submit">
