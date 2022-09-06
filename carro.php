26ada1a7a36bc5033a05a11d6f7d6ed43a654888
Heric <heric.ribeiro@alunos.sc.senac.br>
2022-09-06 16:48:51 -0300

    adicionando javascript

--- carro.php
+++ carro.php
@@ -44,6 +44,26 @@ $sql_code ="SELECT * FROM carros WHERE imagem"
 
     </section>
 
+    <script>
+        var MainImg = document.getElementById('MainImg');
+        var smallimg = document.getElementsByClassName('small-img');
+
+        smallimg[0].onclick = function () {
+            MainImg.src = smallimg[0].src;
+        }
+        smallimg[1].onclick = function () {
+            MainImg.src = smallimg[1].src;
+        }
+        smallimg[2].onclick = function () {
+            MainImg.src = smallimg[2].src;
+        }
+        smallimg[3].onclick = function () {
+            MainImg.src = smallimg[3].src;
+        }
+
+
+
+    </script>
 
 <?php 
 include "footer.php";
