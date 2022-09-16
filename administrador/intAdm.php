3062f6c573d6a1f2ee0490f0e80ff55f29becd69
AugustoCesa <101429053+AugustoCesa@users.noreply.github.com>
2022-09-16 10:38:31 -0300

    .

---
+++ administrador/intAdm.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interface de administrador</title>
+</head>
+
+<body style="background-image: linear-gradient(#2046b0,#101c66); margin:0 auto; width:100%;">
+
+
+
+
+    <h1 style="color: white; text-align:left; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; width:100%">Interface de administrador</h1>
+    <hr>
+    <div style="display: flex; justify-content:center; margin-top: 100px; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; width:100% ">
+        <div style="display: flex; flex-direction:column; color:white">
+            <div style="border: solid black 2px; border-radius:8px; margin-top: 40px; text-align:center;height:350px; width: 400px; width:100%">
+               <h2>Carros</h2>
+            <h3 style="color: white;">Cadastrar Carro</h3>
+            <a href="cadCarros.php"><button type="button"  style="width: 80px;height:30px; background-color:gray; color:white; font-size:20px">entrar</button></a>
+                <h3 style="color: white;">Mudar informações dos carros</h3>
+                <a href="int_altCarro.php"><button type="button"  style="width: 80px;height:30px; background-color:gray; color:white; font-size:20px">entrar</button></a>
+                <h3 style="color: white;">Lista de carros</h3>
+                <a href="listCarro.php"><button type="button"  style="width: 80px;height:30px; background-color:gray; color:white; font-size:20px">entrar</button></a>
+            </div>
+            <div style="border: solid black 2px; border-radius:8px; margin-top: 40px; text-align:center; height:190px; width:100%">
+                <h2 style="color: white;">Usuário</h2>
+                <h3 style="color:white;">Alterar usuários</h3>
+                <a href="listUsuario.php"><button type="button"  style="width: 80px;height:30px; background-color:gray; color:white; font-size:20px">entrar</button></a>
+            </div>
+
+            <h3><a href="../index.php" style="text-decoration: none; font-size:30px; border: solid white 2px; border-radius:4px; color:white">voltar para o inicio</a></h3>
+
+        </div>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
