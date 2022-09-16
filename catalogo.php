89b036688912c561817694aae19cbdbef216edfb
Augusto <augusto.boebel@alunos.sc.senac.br>
2022-09-16 14:55:07 -0300

    .

--- catalogo.php
+++ catalogo.php
@@ -3,9 +3,13 @@
 $strcon = mysqli_connect('localhost', 'root', '', 'projeto') or die("Erro ao conectar o banco");
 $sql = "SELECT * FROM CARROS";
 $resultado = mysqli_query($strcon, $sql) or die('Erro ao consultar o catálgo');
+?>
 
+<div class="container py-5">
+    <div class="row mt-3">
+<?php
 if (mysqli_num_rows($resultado) > 0) {
-
+    
     while ($produtos = mysqli_fetch_array($resultado)) {
 
 
@@ -19,14 +23,13 @@ if (mysqli_num_rows($resultado) > 0) {
 
         $categoria = $produtos['categoria'];
 
-        $imagem = $produtos['imagem']
+        $imagem = $produtos['imagem'];
+
+        /////////
 ?>
-
-        <div class="container py-5">
-            <div class="row mt-3">
-
-
-                <div class="container resultado" style=" border:solid white 10px;display: flex; flex-direction: column; margin-top: 100px; font-size:22px; background-color:white; width: 300px; height: 470px; border-radius: 8px;">
+        
+            
+                <div class="container_catalogo" style=" border:solid white 10px; margin-top: 50px; font-size:22px; background-color:white; width: 300px; height: 450px; border-radius: 8px; margin-left:15px;; margin-right: 15px ;">
 
                     <img style="width:240px; border-radius:11px" src="data:image/jpg;charset=utf8;base64, <?php echo base64_encode($imagem) ?>" class="img-fluid">
                     <p><strong> Marca: <?php echo $marca; ?></Strong></p>
@@ -50,12 +53,15 @@ if (mysqli_num_rows($resultado) > 0) {
                 </div>
 
 
-            </div>
-        </div>
-
+            
+        
 <?php
-
+        /////////
     }
+?>
+        </div>
+    </div>
+<?php
 } else {
     echo "Sem conteudo";
 }
